e5d17d4edebbe990854f263e07a1f23e33a60d68
Samuel <samuel.volpato@pg.inatel.br>
2022-10-08 16:05:24 -0300

    feat: mostra pagina offline

---
+++ sw.js
@@ -0,0 +1,34 @@
+const staticCache = 'app-shell-v1';
+
+const assetsToCache = ["offline.html"];
+
+async function cacheStaticAssets() {
+    const cache = await caches.open(staticCache);
+    return cache.addAll(assetsToCache);
+}
+
+async function networkFirst(request) {
+    try {
+        return await fetch(request);
+    } catch (error) {
+        console.log("[Service Worker] network error", error);
+        const cache = await caches.open(staticCache);
+        return cache.match('offline.html');
+    }
+}
+
+self.addEventListener('install', (event) => {
+    console.log('[Servvice Worker] Installing service worker');
+    event.waitUntil(cacheStaticAssets());
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', () => {
+    console.log('[Service Worker] Activating service worker');
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('[Service Worker] Fetch event worker!', event.request.url);
+    event.respondWith(networkFirst(event.request));
+});
